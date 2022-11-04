ca715e32c9d125b8f2c45c0c98b28cc3b910876a
Yauheni Rudzko <e-rudko2000@mail.ru>
2022-11-04 10:58:00 +0300

    Remove local maxima

--- src/main/java/com/epam/rd/autotasks/arrays/LocalMaximaRemove.java
+++ src/main/java/com/epam/rd/autotasks/arrays/LocalMaximaRemove.java
@@ -11,9 +11,15 @@ public class LocalMaximaRemove {
     }
 
     public static int[] removeLocalMaxima(int[] array){
+        int[] result = new int[array.length];
+        int count = 0;
 
-        //put your code here
-
-        throw new UnsupportedOperationException();
+        for (int i = 0; i < array.length; i++) {
+            if ((i > 0 && array[i] <= array[i - 1])
+                    || (i != array.length - 1 && array[i] <= array[i + 1])){
+                result[count++] = array[i];
+            }
+        }
+        return Arrays.copyOf(result, count);
     }
 }
